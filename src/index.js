9f75cd0bf77629c82f5775919017f359f581d82f
Scagender <scagender@scagender>
2024-09-13 18:17:51 -0300

    Primer push

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const Koa = require('koa');
+const Router = require('@koa/router');
+const cors = require('@koa/cors');
+const app = new Koa();
+const router = new Router();
+
+// Definir rutas
+router.get('/', async ctx => {
+  ctx.body = 'Ruta principal de la API';
+});
+
+router.get('/saludo', async ctx => {
+  ctx.body = '¡Hola desde la API Koa!';
+});
+
+app.use(cors());
+
+// Usar el router en la app
+app.use(router.routes()).use(router.allowedMethods());
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Servidor Koa corriendo en http://localhost:${port}`);
+});
