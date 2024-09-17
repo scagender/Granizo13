aff89130a760b741ac928197487c575b7c6a9370
stefanocando <stefano.cando@uc.cl>
2024-09-17 14:44:22 -0300

    Added health route

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ const Router = require('@koa/router');
 const router = new Router();
 
 const orders = require('./routes/orders');
+const { server } = require('./app');
 
 
 router.get('/', async ctx => {
@@ -10,5 +11,21 @@ router.get('/', async ctx => {
 
 router.use('/orders', orders.routes());
 
+async function checkDB(ctx) {
+  try {
+    return true;
+  } catch (error) {
+    ctx.status = 500;
+  }
+}
+
+router.get('/health', async ctx => {
+  const dbStatus = await checkDB(ctx);
+  ctx.status = dbStatus? 200: 500;
+  ctx.body = { 
+    db: dbStatus? 'ok': 'error',
+    server: 'ok'
+  };  
+});
 
 module.exports = router;
